6317abc2cb26bc8b32eb43f842909007127bba08
Kapil-Baser <kapil.kumar.baser@gmail.com>
2025-10-29 16:12:44 +0530

    Movie details pojo

---
+++ src/main/java/com/example/movieapi/model/Movie.java
@@ -0,0 +1,29 @@
+package com.example.movieapi.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Movie {
+
+    @JsonProperty("adult")
+    private boolean adult;
+    @JsonProperty("id")
+    private long id;
+    @JsonProperty("imdb_id")
+    private String imdbId;
+    @JsonProperty("original_language")
+    private String originalLanguage;
+    @JsonProperty("original_title")
+    private String originalTitle;
+    @JsonProperty("overview")
+    private String overview;
+    @JsonProperty("release_date")
+    private LocalDate releaseDate;
+    @JsonProperty("runtime")
+    private int runtime;
+}
