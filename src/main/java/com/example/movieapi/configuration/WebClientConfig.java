1bc9086babacf08b9fde7763630ca0bb2b544c99
Kapil-Baser <kapil.kumar.baser@gmail.com>
2025-10-31 10:38:49 +0530

    Custom config for RestClient

--- src/main/java/com/example/movieapi/configuration/WebClientConfig.java
+++ src/main/java/com/example/movieapi/configuration/WebClientConfig.java
@@ -3,8 +3,6 @@ package com.example.movieapi.configuration;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.ClientHttpRequestFactory;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 
 @Configuration
