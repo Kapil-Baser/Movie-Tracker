c334af817e2afed70bc5886d61d1cb2741166e06
Kapil-Baser <kapil.kumar.baser@gmail.com>
2025-11-01 14:59:48 +0530

    Database entity for genres. This will be used to store all the genres into database and then just resuing that instead of calling the api next time.

---
+++ src/main/java/com/example/movieapi/entity/Genre.java
@@ -0,0 +1,34 @@
+package com.example.movieapi.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "genres")
+@Data
+public class Genre {
+    @Id
+    private int id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @ManyToMany(mappedBy = "genres")
+    @JsonIgnore
+    private Set<Movie> movies = new HashSet<>();
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "modified_at")
+    private LocalDateTime modifiedAt;
+}
