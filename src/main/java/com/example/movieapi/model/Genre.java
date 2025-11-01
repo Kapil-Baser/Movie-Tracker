771dc8284f697b2d33dcfe21f68eb31449db376c
Kapil-Baser <kapil.kumar.baser@gmail.com>
2025-11-01 14:58:48 +0530

    Genre response pojo

---
+++ src/main/java/com/example/movieapi/model/Genre.java
@@ -0,0 +1,13 @@
+package com.example.movieapi.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class Genre {
+    private int id;
+    private String name;
+}
