998742859d17f6c3e3a43bbb3885c770c51fa6e8
Kapil-Baser <kapil.kumar.baser@gmail.com>
2025-11-06 16:24:54 +0530

    POJO for release dates

---
+++ src/main/java/com/example/movieapi/model/TmdbCountryRelease.java
@@ -0,0 +1,18 @@
+package com.example.movieapi.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class TmdbCountryRelease {
+
+    @JsonProperty("iso_3166_1")
+    private String countryCode;
+
+    @JsonProperty("release_dates")
+    private List<TmdbReleaseDate> releaseDates;
+}
