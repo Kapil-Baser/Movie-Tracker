9d2057c33e894272dd8dc20bd8be08126d98191e
Kapil-Baser <kapil.kumar.baser@gmail.com>
2025-11-06 16:24:39 +0530

    POJO for release dates

---
+++ src/main/java/com/example/movieapi/model/TmdbReleaseDate.java
@@ -0,0 +1,27 @@
+package com.example.movieapi.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class TmdbReleaseDate {
+
+    @JsonProperty("certification")
+    private String certification;
+
+    @JsonProperty("iso_639_1")
+    private String languageCode;
+
+    @JsonProperty("release_date")
+    private LocalDateTime releaseDate;
+
+    @JsonProperty("type")
+    private Integer type;
+
+    @JsonProperty("note")
+    private String note;
+}
