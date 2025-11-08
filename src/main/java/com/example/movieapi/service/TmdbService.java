fbeac1d8bfd268894e7818f3edf2bd2569141b2e
Kapil-Baser <kapil.kumar.baser@gmail.com>
2025-11-08 12:38:43 +0530

    More customized method to get upcoming movies than the default one provided by tmdb.

--- src/main/java/com/example/movieapi/service/TmdbService.java
+++ src/main/java/com/example/movieapi/service/TmdbService.java
@@ -3,9 +3,13 @@ package com.example.movieapi.service;
 import com.example.movieapi.model.MovieResult;
 import com.example.movieapi.model.TmdbReleaseDatesResponse;
 import com.example.movieapi.model.TmdbUpcomingResponse;
+import com.example.movieapi.model.response.MovieResultResponse;
+import com.example.movieapi.model.response.TmdbConfigurationResponse;
+import com.example.movieapi.model.response.TmdbDiscoverResponse;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Objects;
 
@@ -18,14 +22,14 @@ public class TmdbService {
         this.restClient = restClient;
     }
 
-    public List<MovieResult> getUpcomingMovies() {
+/*    public List<MovieResultResponse> getUpcomingMovies() {
         TmdbUpcomingResponse response = restClient.get()
                 .uri(uriBuilder -> uriBuilder.path("/movie/upcoming").build())
                 .retrieve()
                 .body(TmdbUpcomingResponse.class);
 
         return Objects.requireNonNull(response).getMovieResults();
-    }
+    }*/
 
     /*
         This method fetches the release dates of a movie by its id.
@@ -37,5 +41,40 @@ public class TmdbService {
                 .body(TmdbReleaseDatesResponse.class);
 
         return Objects.requireNonNull(response);
+    }
+
+    public TmdbDiscoverResponse getTrendingHorrorMovies() {
+        TmdbDiscoverResponse response = restClient.get()
+                .uri(uriBuilder -> uriBuilder.path("discover/movie")
+                        .queryParam("with_original_language", "en")
+                        .queryParam("with_genres", "27")
+                        .build())
+                .retrieve()
+                .body(TmdbDiscoverResponse.class);
+
+        return Objects.requireNonNull(response);
+    }
+
+    public TmdbConfigurationResponse getConfigurations() {
+        TmdbConfigurationResponse response = restClient.get()
+                .uri(uriBuilder -> uriBuilder.path("/configuration").build())
+                .retrieve()
+                .body(TmdbConfigurationResponse.class);
+
+        return Objects.requireNonNull(response);
+    }
+
+    public List<MovieResultResponse> getUpcomingMovies() {
+        TmdbDiscoverResponse response = restClient.get()
+                .uri(uriBuilder -> uriBuilder.path("discover/movie")
+                        .queryParam("with_original_language", "en")
+                        .queryParam("primary_release_year", LocalDate.now().getYear())
+                        .queryParam("primary_release_date.gte", LocalDate.now().withDayOfMonth(1))
+                        .queryParam("primary_release_date.lte", LocalDate.now().plusWeeks(3))
+                        .build())
+                .retrieve()
+                .body(TmdbDiscoverResponse.class);
+
+        return Objects.requireNonNull(response).getResults();
     }
 }
