836dc7449fcaf92ffb6e6b9f3e32a489fa9e05d8
Kapil-Baser <kapil.kumar.baser@gmail.com>
2025-11-08 12:37:50 +0530

    Method to find all genres by movie ids

--- src/main/java/com/example/movieapi/repository/GenresRepository.java
+++ src/main/java/com/example/movieapi/repository/GenresRepository.java
@@ -5,9 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+import java.util.Set;
 
 @Repository
 public interface GenresRepository extends JpaRepository<Genre, Integer> {
 
     Optional<Genre> findByName(String name);
+
+    Set<Genre> findByIdIn(Set<Integer> genreIds);
 }
