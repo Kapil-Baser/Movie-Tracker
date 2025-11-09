525991e4220bfa8e92a189fbd2d139238fe4e006
Kapil-Baser <kapil.kumar.baser@gmail.com>
2025-11-09 13:36:06 +0530

    Movie dto

---
+++ src/main/java/com/example/movieapi/dto/MovieDto.java
@@ -0,0 +1,10 @@
+package com.example.movieapi.dto;
+
+import lombok.Builder;
+
+import java.util.Set;
+
+@Builder
+public record MovieDto(String title, String overview, Set<String> genres, String backdropPath,
+                       String posterPath, String usDigitalReleaseDate) {
+}
