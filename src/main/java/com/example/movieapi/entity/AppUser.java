6d2d6c127e03ab3b350042c10c377279ba7a6016
Kapil-Baser <kapil.kumar.baser@gmail.com>
2025-11-17 06:42:27 +0530

    User entity

---
+++ src/main/java/com/example/movieapi/entity/AppUser.java
@@ -0,0 +1,41 @@
+package com.example.movieapi.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "users")
+public class AppUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    private boolean enable;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "role_id", nullable = false)
+    private Role role;
+
+    public AppUser(String username, String email, String password, boolean enable, Role role) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+        this.enable = enable;
+    }
+}
