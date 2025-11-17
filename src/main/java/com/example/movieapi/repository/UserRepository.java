26f835d43157e0e8028563210ca1dc56545dc938
Kapil-Baser <kapil.kumar.baser@gmail.com>
2025-11-18 03:54:44 +0530

    User repo

---
+++ src/main/java/com/example/movieapi/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.example.movieapi.repository;
+
+import com.example.movieapi.entity.AppUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<AppUser, Long> {
+    Optional<AppUser> findByEmail(String email);
+
+    Optional<AppUser> findByUsername(String username);
+}
