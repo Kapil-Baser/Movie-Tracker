eafea0a4cda124c2c4ea52e333f83ac5bc91b5c5
Kapil-Baser <kapil.kumar.baser@gmail.com>
2025-11-29 05:25:21 +0530

    dto for movie collection

---
+++ src/main/java/com/example/movieapi/dto/CollectionDto.java
@@ -0,0 +1,19 @@
+package com.example.movieapi.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CollectionDto {
+
+    @NotBlank(message = "Name cannot be blank or empty.")
+    @Size(min = 6, max = 255, message = "Collection name must be between 6 and 255 characters.")
+    private String name;
+}
