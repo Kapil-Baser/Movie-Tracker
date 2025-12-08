f0d0040b9114b9d31189c3cd9cbacdcd019b269a
Kapil-Baser <kapil.kumar.baser@gmail.com>
2025-12-08 14:32:22 +0530

    Controller for favorite movies collection

---
+++ src/main/java/com/example/movieapi/controller/FavoriteCollectionController.java
@@ -0,0 +1,35 @@
+package com.example.movieapi.controller;
+
+import com.example.movieapi.model.AuthenticatedUser;
+import com.example.movieapi.service.MovieCollectionService;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/collections/favorites")
+public class FavoriteCollectionController {
+
+    private final MovieCollectionService collectionService;
+
+    public FavoriteCollectionController(MovieCollectionService collectionService) {
+        this.collectionService = collectionService;
+    }
+
+
+    @PostMapping("/{movie_id}")
+    public String addOrRemoveFromFavorites(@PathVariable(name = "movie_id") Long movieId,
+                                 @AuthenticationPrincipal AuthenticatedUser authenticatedUser,
+                                 Model model) {
+
+        boolean isFavorited =  collectionService.toggleFavorite(authenticatedUser, movieId);
+
+        model.addAttribute("movieId", movieId);
+        model.addAttribute("isFavorited", isFavorited);
+
+        return "fragments/buttons :: fav-button";
+    }
+}
