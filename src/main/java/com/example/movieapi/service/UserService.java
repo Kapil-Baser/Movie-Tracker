9d36e85c35a2a981e25c656e36702d6fa8566248
Kapil-Baser <kapil.kumar.baser@gmail.com>
2025-12-10 12:37:38 +0530

    Adding event publisher and change password and perform a logout upon a successfull password change to invalidate the current session

--- src/main/java/com/example/movieapi/service/UserService.java
+++ src/main/java/com/example/movieapi/service/UserService.java
@@ -2,18 +2,23 @@ package com.example.movieapi.service;
 
 import com.example.movieapi.dto.RegisterUserDto;
 import com.example.movieapi.entity.AppUser;
+import com.example.movieapi.entity.PasswordResetToken;
+import com.example.movieapi.event.TokenResendEvent;
+import com.example.movieapi.event.UserRegisteredEvent;
 import com.example.movieapi.model.AuthenticatedUser;
 import com.example.movieapi.repository.RoleRepository;
 import com.example.movieapi.repository.UserRepository;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
 
 @Service
 @Slf4j
@@ -22,27 +27,39 @@ public class UserService implements UserDetailsService {
     private final UserRepository repository;
     private final RoleRepository roleRepository;
     private final PasswordEncoder passwordEncoder;
+    private final PasswordResetTokenService passwordResetTokenService;
+    private final ApplicationEventPublisher eventPublisher;
 
     @Autowired
-    public UserService(UserRepository repository, RoleRepository roleRepository, PasswordEncoder passwordEncoder) {
+    public UserService(UserRepository repository, RoleRepository roleRepository, PasswordEncoder passwordEncoder, PasswordResetTokenService passwordResetTokenService, ApplicationEventPublisher eventPublisher) {
         this.repository = repository;
         this.roleRepository = roleRepository;
         this.passwordEncoder = passwordEncoder;
+        this.passwordResetTokenService = passwordResetTokenService;
+        this.eventPublisher = eventPublisher;
     }
 
-    public Optional<AppUser> loadUserByEmail(String email) {
-        return repository.findByEmail(email);
+    public AppUser loadUserByEmail(String email) {
+        return repository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Unregistered email"));
     }
 
-    public AppUser registerUser(RegisterUserDto dto) {
-        AppUser newUser = new AppUser(dto.getUsername(),
+    public boolean existsByEmail(String email) {
+        return repository.existsByEmail(email);
+    }
+
+    public void registerUser(RegisterUserDto dto) {
+        AppUser newUser = new AppUser(
+                dto.getUsername(),
                 dto.getEmail(),
                 passwordEncoder.encode(dto.getPassword()),
-                true,
+                false,
                 roleRepository.findByName("ROLE_USER")
                 );
 
-        return repository.save(newUser);
+        AppUser savedUser = repository.save(newUser);
+
+        eventPublisher.publishEvent(new UserRegisteredEvent(savedUser));
     }
 
     @Override
@@ -52,5 +69,33 @@ public class UserService implements UserDetailsService {
         var authenticatedUser = new AuthenticatedUser(user);
         log.info("Username {} authorities {}", authenticatedUser.getUser(), authenticatedUser.getAuthorities());
         return authenticatedUser;
+    }
+
+    public void changePassword(AuthenticatedUser authenticatedUser, String newPassword) {
+
+        AppUser user = authenticatedUser.getUser();
+
+        user.setPassword(passwordEncoder.encode(newPassword));
+
+        repository.save(user);
+
+    }
+
+    public void performLogout(HttpServletRequest request) {
+        SecurityContextLogoutHandler logoutHandler = new SecurityContextLogoutHandler();
+        logoutHandler.logout(request, null, null);
+    }
+
+    public void resendToken(String email) {
+        AppUser user = loadUserByEmail(email);
+
+        eventPublisher.publishEvent(new TokenResendEvent(user));
+    }
+
+    public void resetPassword(String email) {
+        AppUser user = loadUserByEmail(email);
+        // Generate password reset token
+        PasswordResetToken token = passwordResetTokenService.generatePasswordResetToken(user);
+        // TODO: publish reset password event
     }
 }
