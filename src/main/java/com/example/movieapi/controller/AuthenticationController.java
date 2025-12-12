f84fa4293dd626354c3db524ce7d278604b2238c
Kapil-Baser <kapil.kumar.baser@gmail.com>
2025-12-12 08:27:49 +0530

    Took out the confirm email logic out of here

--- src/main/java/com/example/movieapi/controller/AuthenticationController.java
+++ src/main/java/com/example/movieapi/controller/AuthenticationController.java
@@ -1,15 +1,21 @@
 package com.example.movieapi.controller;
 
-import com.example.movieapi.dto.RegisterUserDto;
-import com.example.movieapi.service.RegisterUserValidator;
-import com.example.movieapi.service.UserService;
+import com.example.movieapi.dto.*;
+import com.example.movieapi.entity.AppUser;
+import com.example.movieapi.event.PasswordResetEvent;
+import com.example.movieapi.exception.ExpiredTokenException;
+import com.example.movieapi.exception.InvalidTokenException;
+import com.example.movieapi.exception.UserAlreadyVerifiedException;
+import com.example.movieapi.service.*;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/auth")
@@ -17,59 +23,84 @@ import org.springframework.web.bind.annotation.*;
 public class AuthenticationController {
 
     private final UserService userService;
-    private final RegisterUserValidator userValidator;
+    private final TokenService tokenService;
+    private final PasswordResetTokenService passwordResetTokenService;
+    private final ApplicationEventPublisher eventPublisher;
+    private static final String REGISTRATION_PAGE = "register";
+    private static final String FORGET_PASSWORD_PAGE = "forget-password";
 
     @Autowired
-    public AuthenticationController(UserService userService, RegisterUserValidator userValidator) {
+    public AuthenticationController(UserService userService, TokenService tokenService, PasswordResetTokenService passwordResetTokenService, ApplicationEventPublisher eventPublisher) {
         this.userService = userService;
-        this.userValidator = userValidator;
+        this.tokenService = tokenService;
+        this.passwordResetTokenService = passwordResetTokenService;
+        this.eventPublisher = eventPublisher;
     }
 
     @GetMapping("/login")
     public String login(@RequestParam(value = "error", required = false) String error, Model model) {
+
         if (error != null) {
-            model.addAttribute("loginError", "Invalid username or password!");
+            if (error.equals("disabled")) {
+                model.addAttribute("loginError", "Your account has not been verified yet. Please check your email.");
+            } else {
+                model.addAttribute("loginError", "Invalid username or password!");
+            }
         }
+
         return "login";
     }
 
-    @GetMapping("/registerUser")
-    public String showRegisterPage(Model model) {
-        model.addAttribute("user", new RegisterUserDto());
-        return "registerUser";
+    @GetMapping("/forgetPassword")
+    public String showForgetPasswordForm(@ModelAttribute("forgetPasswordDto") ForgetPasswordDto dto) {
+
+        return FORGET_PASSWORD_PAGE;
     }
 
-    @PostMapping("/registerUser/save")
-    public String registration(@Valid @ModelAttribute("user") RegisterUserDto userDto,
-                               BindingResult result,
-                               Model model) {
+    @PostMapping("/forgetPassSendMail")
+    public String sendResetPasswordEmail(@Valid @ModelAttribute("forgetPasswordDto") ForgetPasswordDto dto,
+                                BindingResult result,
+                                RedirectAttributes redirectAttributes) {
 
-        // Validation
-        userValidator.validate(userDto, result);
-
+        // Returning early in case email is incorrect
+        // If I don't then this error gets added with "No account exists"
         if (result.hasErrors()) {
-            return "registerUser";
+            return FORGET_PASSWORD_PAGE;
         }
 
-        try {
-
-            userService.registerUser(userDto);
-            log.info("User successfully registered: {}", userDto.getUsername());
-
-            return "registerUser-success";
-        } catch (Exception e) {
-
-            log.warn("Exception: {}", e.getMessage());
-            model.addAttribute("error", "Registration failed. Please try again.");
-
-            return "registerUser";
+        if (userService.existsByEmail(dto.email())) {
+            // Publish Password Reset Event
+            eventPublisher.publishEvent(new PasswordResetEvent(dto.email()));
         }
 
+        RedirectInfo redirectInfo = new RedirectInfo("Success", "If an account exists with this email then we have sent a password reset link");
 
+        redirectAttributes.addFlashAttribute("redirectInfo", redirectInfo);
+
+        return "redirect:/auth/login";
     }
 
-/*    @PostMapping("/registerUser")
-    public ResponseEntity<AppUser> registerUser(@RequestBody RegisterUserDto registerUserDto) {
-        return ResponseEntity.ok(userService.registerUser(registerUserDto));
-    }*/
+    @GetMapping("/resetPassword")
+    public String passwordResetPage(@RequestParam("token") String token,
+                                    @Valid @ModelAttribute("passwordResetDto") ResetPasswordDto dto,
+                                    BindingResult result,
+                                    RedirectAttributes redirectAttributes,
+                                    Model model) {
+        // TODO: validate password dto
+        try {
+            passwordResetTokenService.validateToken(token);
+
+        } catch (ExpiredTokenException | InvalidTokenException _) {
+            RedirectInfo redirectInfo = new RedirectInfo("Error", "Invalid or expired token");
+            redirectAttributes.addFlashAttribute("redirectInfo", redirectInfo);
+
+            return "redirect:/auth/login";
+        }
+        if (result.hasErrors()) {
+            return "reset-token-error";
+        }
+        // Validate the token
+
+        return "reset-token-error";
+    }
 }
