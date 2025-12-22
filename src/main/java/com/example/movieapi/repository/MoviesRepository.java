38073c18751e6e4dcd1596e818a051957d37f281
Kapil-Baser <kapil.kumar.baser@gmail.com>
2025-12-22 15:08:26 +0530

    custom query to get paged movies by collection id

--- src/main/java/com/example/movieapi/repository/MoviesRepository.java
+++ src/main/java/com/example/movieapi/repository/MoviesRepository.java
@@ -1,8 +1,11 @@
 package com.example.movieapi.repository;
 
 import com.example.movieapi.entity.Movie;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,6 +15,11 @@ public interface MoviesRepository extends JpaRepository<Movie, Long> {
 
     List<Movie> findByTitleContainingIgnoreCase(String title);
 
+    List<Movie> findByUsDigitalDateIsNull();
+
     @Query("SELECT m.id FROM Movie m")
     List<Long> findAllMovieIds();
+
+    @Query("SELECT m FROM MovieCollection c JOIN c.movies m WHERE c.id = :collectionId")
+    Page<Movie> findMoviesByCollectionId(@Param("collectionId") Long collectionId, Pageable pageable);
 }
