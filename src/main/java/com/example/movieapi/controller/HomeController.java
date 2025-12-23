f117e5c8710fad63893ec4b969eb6e5ca13328eb
Kapil-Baser <kapil.kumar.baser@gmail.com>
2025-12-23 18:43:44 +0530

    ...

--- src/main/java/com/example/movieapi/controller/HomeController.java
+++ src/main/java/com/example/movieapi/controller/HomeController.java
@@ -1,37 +1,80 @@
 package com.example.movieapi.controller;
 
-import com.example.movieapi.dto.MovieDetailsDto;
-import com.example.movieapi.model.PagedResults;
+import com.example.movieapi.dto.MovieDto;
+import com.example.movieapi.entity.AppUser;
+import com.example.movieapi.model.AuthenticatedUser;
+import com.example.movieapi.service.MovieCollectionService;
 import com.example.movieapi.service.MovieService;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@RestController
-@RequestMapping("/api/v1")
+import java.util.List;
+import java.util.Set;
+
+@Controller
+@RequestMapping("/movies")
 public class HomeController {
 
+    private final MovieCollectionService collectionService;
     private final MovieService movieService;
 
-    public HomeController(MovieService movieService) {
+    @Autowired
+    public HomeController(MovieCollectionService collectionService, MovieService movieService) {
+        this.collectionService = collectionService;
         this.movieService = movieService;
     }
 
-    @GetMapping("/home")
-    public String home() {
-        return "Hello! This is movie database api.";
+    /**
+     * Pre-populating the view with user's favorited movie ids in case user is logged in
+     * @param authenticatedUser to check if user is logged in
+     * @param model to add favorites movies ids and now playing movies back to template
+     */
+    @ModelAttribute
+    public void addFavoritedAndWatchedMovieIds(@AuthenticationPrincipal AuthenticatedUser authenticatedUser, Model model) {
+
+        if (authenticatedUser != null) {
+            AppUser user = authenticatedUser.getUser();
+            Set<Long> favoritedMovieIds = collectionService.getFavoritedMovieIds(user);
+            Set<Long> watchedMovieIds = collectionService.getWatchedMovieIds(user);
+
+            model.addAttribute("favoritedMovieIds", favoritedMovieIds);
+            model.addAttribute("watchedMovieIds", watchedMovieIds);
+
+            model.addAttribute("username", user.getUsername());
+        }
     }
 
-    @GetMapping("/movie/{movie_id}")
-    public MovieDetailsDto getMovieDetails(@PathVariable("movie_id") Long movieId) {
-        return movieService.getMovieTopLevelDetailsById(movieId);
+    /**
+     * Home page
+     * @param model to add now-playing movies as view
+     * @return Now playing movies page
+     */
+
+    @GetMapping()
+    public String nowPlaying(Model model) {
+        List<MovieDto> nowPlaying = collectionService.getAllMoviesFromCollection("Now Playing");
+        model.addAttribute("movies", nowPlaying);
+        return "now-playing";
+    }
+
+    @GetMapping("/upcoming")
+    public String upcoming(Model model) {
+        List<MovieDto> upcoming = collectionService.getAllMoviesFromCollection("Upcoming");
+        model.addAttribute("movies", upcoming);
+        return "upcoming";
     }
 
     @GetMapping("/search")
-    public PagedResults searchTitle(@RequestParam String title) {
-        return movieService.getMovieByTitle(title);
-    }
-
-    @GetMapping("/trending/{time_window}")
-    public PagedResults trendingMovies(@PathVariable("time_window") String timeWindow) {
-        return movieService.getTrendingMovies(timeWindow);
+    public String searchMovieByName(@RequestParam(value = "keyword", required = false) String keyword, Model model) {
+        List<MovieDto> movieDtoList = movieService.getMoviesByKeyword(keyword);
+        model.addAttribute("movies", movieDtoList);
+        model.addAttribute("keyword", keyword);
+        return "search-results";
     }
 }
