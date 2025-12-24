c32ea39c827ef2fde155dafb313466e0b9794eec
Kapil-Baser <kapil.kumar.baser@gmail.com>
2025-12-24 12:40:16 +0530

    Mail service class to send confirmation emails and reset password tokens

---
+++ src/main/java/com/example/movieapi/service/MailService.java
@@ -0,0 +1,40 @@
+package com.example.movieapi.service;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import java.sql.Date;
+import java.time.LocalDate;
+
+@Service
+public class MailService {
+
+    private final JavaMailSender mailSender;
+
+
+    public MailService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    public void sendPlainText(String to, String subject, String body) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(body);
+        message.setSentDate(Date.valueOf(LocalDate.now()));
+        mailSender.send(message);
+    }
+
+    public void sendHtml(String to, String subject, String htmlBody) throws MessagingException {
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, "UTF-8");
+        helper.setTo(to);
+        helper.setSubject(subject);
+        helper.setText(htmlBody, true); // true means this is HTML
+        mailSender.send(message);
+    }
+}
