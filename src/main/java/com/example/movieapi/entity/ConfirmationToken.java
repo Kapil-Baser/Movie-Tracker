1dda33403526e812142ede741bc99f8c664a838e
Kapil-Baser <kapil.kumar.baser@gmail.com>
2025-12-24 12:39:39 +0530

    token created at should never be null

--- src/main/java/com/example/movieapi/entity/ConfirmationToken.java
+++ src/main/java/com/example/movieapi/entity/ConfirmationToken.java
@@ -25,7 +25,7 @@ public class ConfirmationToken {
     private boolean revoked;
 
     @CreationTimestamp
-    @Column(name = "created_at")
+    @Column(name = "created_at", updatable = false)
     private LocalDateTime createdAt;
 
     @Column(name = "expires_at", nullable = false)
