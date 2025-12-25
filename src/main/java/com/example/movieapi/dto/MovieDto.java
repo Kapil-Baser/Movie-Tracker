2686ca110840aad90b5c1917e9eef1bea23d78bd
Kapil-Baser <kapil.kumar.baser@gmail.com>
2025-12-25 13:00:49 +0530

    More data for moviedto to display

--- src/main/java/com/example/movieapi/dto/MovieDto.java
+++ src/main/java/com/example/movieapi/dto/MovieDto.java
@@ -6,5 +6,6 @@ import java.util.Set;
 
 @Builder
 public record MovieDto(Long id, String title, String overview, Set<String> genres, String backdropPath,
-                       String posterPath, String usDigitalReleaseDate, String releaseDate) {
+                       String posterPath, String usDigitalReleaseDate, String releaseDate, String runtime,
+                       String tagline, String imdbId, String trailer) {
 }
