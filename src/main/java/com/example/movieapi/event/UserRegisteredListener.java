929bf38e863264dbe518e41c149ba921c92704f3
Kapil-Baser <kapil.kumar.baser@gmail.com>
2025-12-25 12:58:07 +0530

    user registered event listener

--- src/main/java/com/example/movieapi/event/UserRegisteredListener.java
+++ src/main/java/com/example/movieapi/event/UserRegisteredListener.java
@@ -1,29 +1,26 @@
 package com.example.movieapi.event;
 
-import com.example.movieapi.entity.AppUser;
-import com.example.movieapi.entity.ConfirmationToken;
-import com.example.movieapi.service.TokenService;
+import com.example.movieapi.service.EmailVerificationService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
 public class UserRegisteredListener {
 
-    private final TokenService tokenService;
+    private final EmailVerificationService emailVerificationService;
 
 
-    public UserRegisteredListener(TokenService tokenService) {
-        this.tokenService = tokenService;
+    public UserRegisteredListener(EmailVerificationService emailVerificationService) {
+        this.emailVerificationService = emailVerificationService;
     }
 
     @EventListener
     public void sendConfirmationToken(UserRegisteredEvent event) {
         log.info("Generating token for user: {}", event.user().getUsername());
-        ConfirmationToken token = tokenService.generateConfirmationToken(event.user());
-        log.info("Successfully generated token: {}", token.getToken());
+        String token = emailVerificationService.createToken(event.user().getEmail());
+        log.info("Successfully generated token: {}", token);
         // TODO: Send email to user
 
     }
