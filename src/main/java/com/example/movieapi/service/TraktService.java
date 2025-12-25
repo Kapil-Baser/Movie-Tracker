cfab055c920329bc3c905633dee96bb20b2ee8c9
Kapil-Baser <kapil.kumar.baser@gmail.com>
2025-12-25 18:36:13 +0530

    trakt api service class

---
+++ src/main/java/com/example/movieapi/service/TraktService.java
@@ -0,0 +1,81 @@
+package com.example.movieapi.service;
+
+import com.example.movieapi.model.trakt.model.TraktMovie;
+import com.example.movieapi.model.trakt.response.TraktMostAnticipatedResponse;
+import com.example.movieapi.model.trakt.response.TraktTrendingResponse;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
+
+import java.util.List;
+
+@Service
+public class TraktService {
+
+    private final RestClient traktServiceClient;
+
+
+    public TraktService(@Qualifier("traktServiceClient") RestClient traktServiceClient) {
+        this.traktServiceClient = traktServiceClient;
+    }
+
+    public List<TraktTrendingResponse> getTrendingMovies() {
+        return traktServiceClient
+                .get()
+                .uri(uriBuilder -> uriBuilder.path("/movies/trending")
+                        .queryParam("page", 1 )
+                        .queryParam("limit", 20)
+                        .build())
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .body(new ParameterizedTypeReference<List<TraktTrendingResponse>>() {});
+    }
+
+    public String getTrendingMoviesApi() {
+        return traktServiceClient
+                .get()
+                .uri(uriBuilder -> uriBuilder.path("/movies/trending")
+                        .queryParam("page", 1)
+                        .queryParam("limit", 20)
+                        .build())
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .body(String.class);
+    }
+
+    public List<TraktMovie> getPopularMovies() {
+        return traktServiceClient
+                .get()
+                .uri(uriBuilder -> uriBuilder.path("/movies/popular").build())
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .body(new ParameterizedTypeReference<List<TraktMovie>>() {
+                });
+    }
+
+    public TraktMovie getExtendedMovieDetails(String movieId) {
+        return traktServiceClient.get()
+                .uri(uriBuilder -> uriBuilder.path("/movies/" + movieId).build())
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .body(TraktMovie.class);
+    }
+
+    public String getReleaseDates(String movieId) {
+        return traktServiceClient.get()
+                .uri(uriBuilder -> uriBuilder.path("/movies/" + movieId + "/releases").build())
+                .retrieve()
+                .body(String.class);
+    }
+
+    public List<TraktMostAnticipatedResponse> getAnticipated() {
+        return traktServiceClient.get()
+                .uri("/movies/anticipated")
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .body(new ParameterizedTypeReference<List<TraktMostAnticipatedResponse>>() {
+                });
+    }
+}
