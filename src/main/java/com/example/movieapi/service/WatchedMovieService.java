977054a387506fe6920dcafaf331014eb92a932f
Kapil-Baser <kapil.kumar.baser@gmail.com>
2025-12-27 13:09:06 +0530

    watched movie service class

---
+++ src/main/java/com/example/movieapi/service/WatchedMovieService.java
@@ -0,0 +1,84 @@
+package com.example.movieapi.service;
+
+import com.example.movieapi.dto.WatchedMovieDto;
+import com.example.movieapi.entity.AppUser;
+import com.example.movieapi.entity.Movie;
+import com.example.movieapi.entity.WatchedMovie;
+import com.example.movieapi.model.AuthenticatedUser;
+import com.example.movieapi.repository.WatchedMovieRepository;
+import com.example.movieapi.utility.FormatUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Service
+public class WatchedMovieService {
+
+    private final WatchedMovieRepository watchedMovieRepository;
+    private final MovieService movieService;
+
+    @Autowired
+    public WatchedMovieService(WatchedMovieRepository watchedMovieRepository, MovieService movieService) {
+        this.watchedMovieRepository = watchedMovieRepository;
+        this.movieService = movieService;
+    }
+
+    public boolean toggleWatched(AuthenticatedUser authenticatedUser, Long movieId) {
+        AppUser user = authenticatedUser.getUser();
+        Movie movie = movieService.getMovieById(movieId);
+
+        Optional<WatchedMovie> watchedMovieRecord = watchedMovieRepository.findByUserAndMovie(user, movie);
+
+        if (watchedMovieRecord.isPresent()) {
+            watchedMovieRepository.delete(watchedMovieRecord.get());
+            return false;
+        } else {
+            WatchedMovie watchedMovie = WatchedMovie.builder()
+                    .user(user)
+                    .movie(movie)
+                    .watchedAt(LocalDateTime.now())
+                    .build();
+            watchedMovieRepository.save(watchedMovie);
+            return true;
+        }
+    }
+
+    public Map<LocalDate, List<WatchedMovieDto>> getWatchedMovies(AuthenticatedUser authenticatedUser) {
+        AppUser user = authenticatedUser.getUser();
+
+        List<WatchedMovie> watchedMovies = watchedMovieRepository.findByUserOrderByWatchedAtDesc(user);
+        List<WatchedMovieDto> watchedMoviesDto = watchedMovies.stream()
+                .map(watchedMovie -> {
+                    Movie movie = watchedMovie.getMovie();
+                    return WatchedMovieDto.builder()
+                            .title(movie.getTitle())
+                            .posterPath(movie.getPosterPath())
+                            .movieId(String.valueOf(movie.getId()))
+                            .watchedAt(watchedMovie.getWatchedAt().toLocalDate())
+                            .releaseDate(FormatUtil.formatReleaseDate(movie.getReleaseDate()))
+                            .genres(movie.getTmdbGenres())
+                            .certification(movie.getCertification())
+                            .runtime(FormatUtil.formatRuntime(movie.getRuntime()))
+                            .rating(movie.getRating())
+                            .votes(movie.getVotes())
+                            .build();
+                })
+                .toList();
+
+        // By default, it was not returning the map in descending order
+        return watchedMoviesDto.stream()
+                .collect(Collectors.groupingBy(WatchedMovieDto::getWatchedAt, LinkedHashMap::new, Collectors.toList()));
+    }
+
+    public Set<Long> getWatchedMoviesIds(AppUser user) {
+        List<Movie> watchedMovies = watchedMovieRepository.findAllWatchedMoviesByUser(user);
+
+        return watchedMovies.stream()
+                .map(Movie::getId)
+                .collect(Collectors.toSet());
+    }
+}
