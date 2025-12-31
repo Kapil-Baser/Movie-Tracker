d7f3a3d730e56401fea09ad640259ebdef7f5af3
Kapil-Baser <kapil.kumar.baser@gmail.com>
2025-12-31 07:30:22 +0530

    Controller for showing the watched movies history view and toggle method for toggling the movie in history

--- src/main/java/com/example/movieapi/controller/WatchedHistoryController.java
+++ src/main/java/com/example/movieapi/controller/WatchedHistoryController.java
@@ -1,8 +1,19 @@
 package com.example.movieapi.controller;
 
+import com.example.movieapi.dto.WatchedMovieDto;
+import com.example.movieapi.model.AuthenticatedUser;
 import com.example.movieapi.service.WatchedMovieService;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/movies/history")
@@ -15,5 +26,23 @@ public class WatchedHistoryController {
         this.watchedMovieService = watchedMovieService;
     }
 
+    @GetMapping()
+    public String showWatchedMoviesHistory(@AuthenticationPrincipal AuthenticatedUser authenticatedUser, Model model) {
 
+        Map<LocalDate, List<WatchedMovieDto>> movieHistoryMap = watchedMovieService.getWatchedMovies(authenticatedUser);
+        model.addAttribute("movieHistoryMap", movieHistoryMap);
+        return "watched-history";
+    }
+
+    @PostMapping("/toggle")
+    public String toggleWatched(@RequestParam Long movieId,
+                                @AuthenticationPrincipal AuthenticatedUser authenticatedUser,
+                                Model model) {
+        boolean isWatched = watchedMovieService.toggleWatched(authenticatedUser, movieId);
+
+        model.addAttribute("isWatched", isWatched);
+        model.addAttribute("movieId", movieId);
+
+        return "fragments/buttons :: watched-button";
+    }
 }
