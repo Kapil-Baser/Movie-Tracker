b6ef4df157f6a511636b3aa4b6144e2f58273acb
Kapil-Baser <kapil.kumar.baser@gmail.com>
2026-01-05 20:34:50 +0530

    Adding resilience features for tmdb api calls

---
+++ src/main/java/com/example/movieapi/configuration/ResilienceConfig.java
@@ -0,0 +1,9 @@
+package com.example.movieapi.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.resilience.annotation.EnableResilientMethods;
+
+@EnableResilientMethods
+@Configuration
+public class ResilienceConfig {
+}
