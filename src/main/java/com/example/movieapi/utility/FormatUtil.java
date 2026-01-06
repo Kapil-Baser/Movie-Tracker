ecf112d99961103ce6a5d1ee229dbd987fcf7683
Kapil-Baser <kapil.kumar.baser@gmail.com>
2026-01-06 09:21:49 +0530

    Added a method to convert a formatted date back to just LocalDate

--- src/main/java/com/example/movieapi/utility/FormatUtil.java
+++ src/main/java/com/example/movieapi/utility/FormatUtil.java
@@ -7,12 +7,24 @@ import java.util.Locale;
 
 public class FormatUtil {
 
+    private FormatUtil() {}
+
     public static String formatRuntime(int runtime) {
         Duration duration = Duration.ofMinutes(runtime);
         return String.format("%dh %dmin", duration.toHours(), duration.toMinutesPart());
     }
 
     public static String formatReleaseDate(LocalDate releaseDate) {
-        return releaseDate.format(DateTimeFormatter.ofPattern("MMM d, yyyy", Locale.ENGLISH));
+        if (releaseDate != null) {
+            return releaseDate.format(DateTimeFormatter.ofPattern("MMM d, yyyy", Locale.ENGLISH));
+        }
+        return "Unknown";
+    }
+
+    public static LocalDate parseReleaseDate(String formattedDate) {
+        if (formattedDate != null && !formattedDate.equals("Unknown")) {
+            return LocalDate.parse(formattedDate, DateTimeFormatter.ofPattern("MMM d, yyyy", Locale.ENGLISH));
+        }
+        return null;
     }
 }
