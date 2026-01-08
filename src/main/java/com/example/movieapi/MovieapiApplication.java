5f630168fdecd0834bf5136a0f7f3dc845a4ce68
Kapil-Baser <kapil.kumar.baser@gmail.com>
2026-01-08 15:27:17 +0530

    Removed open feign and now resilience has its own config file

--- src/main/java/com/example/movieapi/MovieapiApplication.java
+++ src/main/java/com/example/movieapi/MovieapiApplication.java
@@ -2,12 +2,8 @@ package com.example.movieapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
-@EnableFeignClients
-@EnableAsync
 public class MovieapiApplication {
 
 	public static void main(String[] args) {
