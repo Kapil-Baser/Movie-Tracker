b8f506370acfb3a71aacf51f8fa2e316e5a47a68
Kapil-Baser <kapil.kumar.baser@gmail.com>
2026-01-09 14:19:17 +0530

    Added another listener for TMDB and now both listener are making use of conditions and running depending on if movies requires to be enriched from TRAKT or TMDB API

--- src/main/java/com/example/movieapi/event/MovieEnrichmentEventListener.java
+++ src/main/java/com/example/movieapi/event/MovieEnrichmentEventListener.java
@@ -1,8 +1,11 @@
 package com.example.movieapi.event;
 
 import com.example.movieapi.entity.Movie;
+import com.example.movieapi.model.TmdbMovie;
+import com.example.movieapi.model.response.TmdbMovieDetailsResponse;
 import com.example.movieapi.model.trakt.model.TraktMovie;
 import com.example.movieapi.service.MovieService;
+import com.example.movieapi.service.TmdbService;
 import com.example.movieapi.service.TraktService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,16 +20,19 @@ import java.util.Objects;
 @Slf4j
 public class MovieEnrichmentEventListener {
     private final TraktService traktService;
+    private final TmdbService tmdbService;
     private final MovieService movieService;
 
     @Autowired
-    public MovieEnrichmentEventListener(TraktService traktService, MovieService movieService) {
+    public MovieEnrichmentEventListener(TraktService traktService, TmdbService tmdbService, MovieService movieService) {
         this.traktService = traktService;
+        this.tmdbService = tmdbService;
         this.movieService = movieService;
     }
 
-    @EventListener
+
     @Async
+    @EventListener(condition = "#event.provider == 'Trakt'")
     public void enrichMoviesFromTrakt(MovieEnrichmentEvent event) {
         List<Movie> moviesToEnrich = event.moviesToEnrich();
 
@@ -46,5 +52,28 @@ public class MovieEnrichmentEventListener {
 
         List<Movie> enrichedMovies = movieService.updateTraktMovies(traktMovies, moviesToEnrich);
         log.info("Enriched {} movies from Trakt API", enrichedMovies.size());
+    }
+
+    @Async
+    @EventListener(condition = "#event.provider == 'TMDB'")
+    public void enrichMoviesFromTmdb(MovieEnrichmentEvent event) {
+        List<Movie> moviesToEnrich = event.moviesToEnrich();
+
+        // Get the imdbIds for further calls
+        List<Long> tmdbIds = moviesToEnrich.parallelStream()
+                .map(Movie::getTmdbId)
+                .filter(Objects::nonNull)
+                .toList();
+        log.info("Requesting details about {} new movies from TMDB API with TMDB IDs: {}", tmdbIds.size(), tmdbIds);
+
+        // Fetching the movie details
+        List<TmdbMovieDetailsResponse> tmdbMovies = tmdbIds.stream()
+                .map(tmdbService::safeGetMovieDetails)
+                .filter(Objects::nonNull)
+                .toList();
+        log.info("Fetched {} out of {} movies from TMDB API", tmdbMovies.size(), tmdbIds.size());
+
+        List<Movie> enrichedMovies = movieService.updateTmdbMovies(tmdbMovies, moviesToEnrich);
+        log.info("Enriched {} movies from TMDB API", enrichedMovies.size());
     }
 }
