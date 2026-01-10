84945b05c6566156813888415341e945532eb47c
Kapil-Baser <kapil.kumar.baser@gmail.com>
2026-01-10 10:36:20 +0530

    Added new methods

--- src/main/java/com/example/movieapi/controller/AdminController.java
+++ src/main/java/com/example/movieapi/controller/AdminController.java
@@ -1,17 +1,17 @@
 package com.example.movieapi.controller;
 
 import com.example.movieapi.dto.MovieDto;
+import com.example.movieapi.model.response.TmdbMovieDetailsResponse;
 import com.example.movieapi.service.MovieSyncService;
+import lombok.NonNull;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/admin")
+@RequestMapping("/api/v1/admin/movie")
 public class AdminController {
 
     private final MovieSyncService movieSyncService;
@@ -25,19 +25,51 @@ public class AdminController {
         return "token";
     }
 
-
-    @PostMapping("/movie/upcoming")
-    public ResponseEntity<List<MovieDto>> upcomingMovies() {
-        return ResponseEntity.ok(movieSyncService.syncUpcomingMoviesToCollection());
+    @PostMapping("/upcoming")
+    public ResponseEntity<List<MovieDto>> upcomingMovies(@RequestParam("page") int page) {
+        return ResponseEntity.ok(movieSyncService.syncUpcomingMovies(page));
     }
 
-    @PostMapping("/movie/sync-release-dates")
+    @PostMapping("/sync-release-dates")
     public ResponseEntity<List<MovieDto>> syncReleaseDates() {
         return ResponseEntity.ok(movieSyncService.syncDigitalReleaseDates());
     }
 
-    @PostMapping("/movie/now-playing")
+    @PostMapping("/now-playing")
     public ResponseEntity<List<MovieDto>> syncNowPlaying() {
         return ResponseEntity.ok(movieSyncService.syncNowPlayingMovies());
+    }
+
+    @GetMapping("/anticipated")
+    public ResponseEntity<List<MovieDto>> syncMostAnticipated() {
+        return ResponseEntity.ok(movieSyncService.syncMostAnticipated());
+    }
+
+    @PostMapping("/now-playing/{page_no}")
+    public ResponseEntity<@NonNull List<@NonNull MovieDto>> syncNowPlayingFromTmdb(@PathVariable(name = "page_no") int page) {
+        List<MovieDto> dto = movieSyncService.syncNowPlayingMoviesFromTmdb(page);
+        if (dto.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return new ResponseEntity<>(dto, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/trending")
+    public ResponseEntity< @NonNull List<MovieDto>> trendingMoviesFromTrakt() {
+        List<MovieDto> dto = movieSyncService.syncTrendingMoviesFromTrakt();
+        if (dto.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(dto);
+    }
+
+    @PostMapping("/update-youtube-trailers")
+    public ResponseEntity<String> updateMoviesWithTrailers() {
+        return ResponseEntity.ok("");
+    }
+    // TODO: change to POST
+    @GetMapping("/details/{movie_id}")
+    public ResponseEntity<TmdbMovieDetailsResponse> getMovieDetails(@PathVariable("movie_id") Long movieId) {
+        return ResponseEntity.ok(movieSyncService.getMovieDetails(movieId));
     }
 }
