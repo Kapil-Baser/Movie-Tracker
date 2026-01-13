3b1c6382a4010ecbdf8595fd3a15004a368ff4b5
Kapil-Baser <kapil.kumar.baser@gmail.com>
2026-01-13 12:51:09 +0530

    ...

--- src/main/java/com/example/movieapi/controller/ApiController.java
+++ src/main/java/com/example/movieapi/controller/ApiController.java
@@ -1,49 +1,26 @@
 package com.example.movieapi.controller;
 
-import com.example.movieapi.dto.MovieDetailsDto;
-import com.example.movieapi.dto.MovieResultDto;
 import com.example.movieapi.model.PagedResults;
 import com.example.movieapi.model.TmdbReleaseDatesResponse;
 import com.example.movieapi.model.response.TmdbDiscoverResponse;
-import com.example.movieapi.service.MovieService;
 import com.example.movieapi.service.TmdbService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1")
 public class ApiController {
 
-    private final MovieService movieService;
     private final TmdbService tmdbService;
 
-    @Autowired
-    public ApiController(MovieService movieService, TmdbService tmdbService) {
-        this.movieService = movieService;
+    public ApiController(TmdbService tmdbService) {
         this.tmdbService = tmdbService;
     }
 
-    @GetMapping("/home")
-    public String home() {
-        return "Hello! This is movie database api.";
-    }
-
-    @GetMapping("/movie/{movie_id}")
-    public MovieDetailsDto getMovieDetails(@PathVariable("movie_id") Long movieId) {
-        return movieService.getMovieTopLevelDetailsById(movieId);
-    }
-
-    @GetMapping("/search")
-    public PagedResults searchTitle(@RequestParam String title) {
-        return movieService.getMovieByTitle(title);
-    }
-
     @GetMapping("/trending/{time_window}")
-    public List<MovieResultDto> trendingMovies(@PathVariable("time_window") String timeWindow) {
-        return movieService.getTrendingMovies(timeWindow);
+    public ResponseEntity<PagedResults> trendingMovies(@PathVariable("time_window") String timeWindow) {
+        return ResponseEntity.ok(tmdbService.getTrendingMoviesByDayOrWeek(timeWindow));
     }
 
     @GetMapping("/movie/release-dates/{movie_id}")
@@ -55,5 +32,4 @@ public class ApiController {
     public ResponseEntity<TmdbDiscoverResponse> trendingHorrorMovies() {
         return ResponseEntity.ok(tmdbService.getTrendingHorrorMovies());
     }
-
 }
