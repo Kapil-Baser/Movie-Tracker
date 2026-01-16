7867f0c0fd32c3735c68bf05d107e4a81dfad728
Kapil-Baser <kapil.kumar.baser@gmail.com>
2026-01-16 15:48:38 +0530

    Reformatted the config and now making use of custom successhandler and custom OAuth2AuthorizationRequestResolver

--- src/main/java/com/example/movieapi/configuration/SecurityConfig.java
+++ src/main/java/com/example/movieapi/configuration/SecurityConfig.java
@@ -8,6 +8,9 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizationRequestResolver;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestResolver;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.csrf.CsrfFilter;
@@ -20,8 +23,76 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
+    // Custom OAuth2 Authorization Request Resolver (for Google login)
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public OAuth2AuthorizationRequestResolver authorizationRequestResolver(
+            ClientRegistrationRepository clientRegistrationRepository) {
+        DefaultOAuth2AuthorizationRequestResolver resolver =
+                new DefaultOAuth2AuthorizationRequestResolver(clientRegistrationRepository, "/oauth2/authorization");
+
+        resolver.setAuthorizationRequestCustomizer(customizer ->
+                customizer.additionalParameters(params -> {
+                    params.put("access_type", "offline");
+                    params.put("prompt", "consent"); // Force consent to ensure refresh token is sent
+                })
+        );
+        return resolver;
+    }
+
+    // Using this so I can show custom errors when authentication fails
+    @Bean
+    public AuthenticationFailureHandler authenticationFailureHandler() {
+        return new CustomAuthenticationFailureHandler();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, OAuth2AuthorizationRequestResolver resolver, CustomOAuth2LoginSuccessHandler successHandler) throws Exception {
+        return http
+                .httpBasic(Customizer.withDefaults())
+                .addFilterAfter(new CsrfLoggingFilter(), CsrfFilter.class)
+
+                .formLogin(form -> form
+                        .loginPage("/auth/login")
+                        .loginProcessingUrl("/auth/login")
+                        .defaultSuccessUrl("/movies")
+                        .failureHandler(authenticationFailureHandler())
+                        .permitAll()
+                )
+
+                .oauth2Login(oauth -> oauth
+                        .authorizationEndpoint(auth -> auth
+                                .authorizationRequestResolver(resolver)
+                        )
+                        .failureHandler(authenticationFailureHandler())
+                        .successHandler(successHandler)
+                )
+
+                .logout(logout -> logout
+                        .logoutSuccessUrl("/movies")
+                        .permitAll()
+                )
+
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/v1/admin/**").hasRole("ADMIN")
+                        .requestMatchers("/users/**").hasRole("USER")
+                        .requestMatchers(
+                                "/auth/**",
+                                "/webjars/**",
+                                "/movies/**",
+                                "/register/**",
+                                "/error"
+                        )
+                                .permitAll()
+                                .anyRequest()
+                                .authenticated()
+                        )
+
+                .build();
+
+    }
+
+    /*@Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, OAuth2AuthorizationRequestResolver resolver, CustomOAuth2LoginSuccessHandler successHandler) throws Exception {
         return http
                 .httpBasic(Customizer.withDefaults())
                 .addFilterAfter(new CsrfLoggingFilter(), CsrfFilter.class)
@@ -31,10 +102,14 @@ public class SecurityConfig {
                         .defaultSuccessUrl("/movies")
                         .failureHandler(authenticationFailureHandler())
                         .permitAll())
+                .oauth2Login(oauth -> oauth.authorizationEndpoint(auth -> auth.authorizationRequestResolver(resolver))
+                    .failureHandler(authenticationFailureHandler())
+                    .defaultSuccessUrl("/movies")
+                        .successHandler(successHandler)
+                )
                 .logout(logout -> logout
                         .logoutSuccessUrl("/movies")
-                        .permitAll()
-                    )
+                        .permitAll())
                 .authorizeHttpRequests(authorize -> {
                     authorize
                             .requestMatchers("/api/v1/admin/**")
@@ -43,17 +118,13 @@ public class SecurityConfig {
                             .hasRole("USER")
                             .requestMatchers("/auth/**")
                             .permitAll()
-                            .requestMatchers("/webjars/**","/movies/**", "/register/**")
+                            .requestMatchers("/webjars/**","/movies/**", "/register/**", "/error")
                             .permitAll()
                             .anyRequest()
                             .authenticated();
                 })
                 .build();
-    }
+    }*/
 
-    // Using this so I can show custom errors when authentication fails
-    @Bean
-    public AuthenticationFailureHandler authenticationFailureHandler() {
-        return new CustomAuthenticationFailureHandler();
-    }
+
 }
