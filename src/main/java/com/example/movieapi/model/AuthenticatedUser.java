408f80fe61394c7f881a08d761d19654612ea713
Kapil-Baser <kapil.kumar.baser@gmail.com>
2026-01-18 12:16:26 +0530

    Added fields required for OAuth2

--- src/main/java/com/example/movieapi/model/AuthenticatedUser.java
+++ src/main/java/com/example/movieapi/model/AuthenticatedUser.java
@@ -3,19 +3,42 @@ package com.example.movieapi.model;
 import com.example.movieapi.entity.AppUser;
 import com.example.movieapi.entity.Role;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.oidc.OidcIdToken;
+import org.springframework.security.oauth2.core.oidc.OidcUserInfo;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 
 @Getter
-@RequiredArgsConstructor
-public class AuthenticatedUser implements UserDetails {
+public class AuthenticatedUser implements UserDetails, OidcUser {
 
     private final AppUser user;
+    private Map<String, Object> attributes;
+    private OidcIdToken idToken;
+    private OidcUserInfo userInfo;
+
+    // Constructor used for Form login
+    public AuthenticatedUser(AppUser user) {
+        this.user = user;
+    }
+
+    // Used for Google login
+    public AuthenticatedUser(AppUser user, Map<String, Object> attributes, OidcIdToken idToken, OidcUserInfo userInfo) {
+        this.user = user;
+        this.attributes = attributes;
+        this.idToken = idToken;
+        this.userInfo = userInfo;
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return attributes;
+    }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
@@ -41,5 +64,28 @@ public class AuthenticatedUser implements UserDetails {
 
     public String getEmail() {
         return user.getEmail();
+    }
+
+    @Override
+    public Map<String, Object> getClaims() {
+        return attributes;
+    }
+
+    @Override
+    public OidcUserInfo getUserInfo() {
+        return userInfo;
+    }
+
+    @Override
+    public OidcIdToken getIdToken() {
+        return idToken;
+    }
+
+    @Override
+    public String getName() {
+        if (idToken != null) {
+            return idToken.getSubject();
+        }
+        return user.getId().toString();
     }
 }
