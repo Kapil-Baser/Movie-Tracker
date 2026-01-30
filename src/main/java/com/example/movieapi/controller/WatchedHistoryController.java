1a11eb08248a38911cd9777b887422abc939e667
Kapil-Baser <kapil.kumar.baser@gmail.com>
2026-01-30 16:26:24 +0530

    Method for marking a movie as unwatched

--- src/main/java/com/example/movieapi/controller/WatchedHistoryController.java
+++ src/main/java/com/example/movieapi/controller/WatchedHistoryController.java
@@ -1,15 +1,15 @@
 package com.example.movieapi.controller;
 
 import com.example.movieapi.dto.WatchedMovieDto;
+import com.example.movieapi.entity.AppUser;
 import com.example.movieapi.model.AuthenticatedUser;
 import com.example.movieapi.service.WatchedMovieService;
+import io.github.wimdeblauwe.htmx.spring.boot.mvc.HtmxResponse;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -44,5 +44,22 @@ public class WatchedHistoryController {
         model.addAttribute("movieId", movieId);
 
         return "fragments/buttons :: mark-as-watched-unwatched-button";
+    }
+
+    @DeleteMapping("/unwatched")
+    public ResponseEntity<Void> markUnwatched(@RequestParam Long movieId,
+                                              @RequestParam LocalDate date,
+                                              @AuthenticationPrincipal AuthenticatedUser authenticatedUser,
+                                              HtmxResponse htmxResponse) {
+        AppUser user = authenticatedUser.getUser();
+        watchedMovieService.markAsUnwatched(user, movieId);
+
+        int moviesWatchedOnDate = watchedMovieService.getWatchedMoviesCount(user, date);
+        if (moviesWatchedOnDate == 0) {
+            // If there are no more movies on the given date then remove the whole container along with date that shows on page
+            htmxResponse.setRetarget("closest .movies-container");
+        }
+
+        return ResponseEntity.ok().build();
     }
 }
