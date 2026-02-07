779b44b3c804a3911cee7f94e308b8a54545459a
Kapil-Baser <kapil.kumar.baser@gmail.com>
2026-02-07 09:41:04 +0530

    Added two more tests. Tests are for validateToken method.

--- src/test/java/com/example/movieapi/service/PasswordResetTokenServiceTest.java
+++ src/test/java/com/example/movieapi/service/PasswordResetTokenServiceTest.java
@@ -2,14 +2,18 @@ package com.example.movieapi.service;
 
 import com.example.movieapi.entity.AppUser;
 import com.example.movieapi.entity.PasswordResetToken;
+import com.example.movieapi.exception.InvalidTokenException;
 import com.example.movieapi.repository.PasswordResetTokenRepository;
+import com.example.movieapi.utility.TokenHashUtil;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
@@ -18,7 +22,7 @@ import static org.mockito.Mockito.*;
 @ExtendWith(MockitoExtension.class)
 class PasswordResetTokenServiceTest {
 
-    private final String VALID_EMAIL = "vaild@example.com";
+    private final String email = "vaild@example.com";
 
     @Mock
     private PasswordResetTokenRepository passwordResetTokenRepository;
@@ -31,10 +35,10 @@ class PasswordResetTokenServiceTest {
     @Test
     void createToken_shouldCreateAndSaveToken_whenUserExists() {
         AppUser appUser = new AppUser();
-        when(userService.loadUserByEmail(VALID_EMAIL)).thenReturn(appUser);
+        when(userService.loadUserByEmail(email)).thenReturn(appUser);
         when(passwordResetTokenRepository.findAllByUserAndConfirmedAtIsNullAndRevokedFalse(appUser)).thenReturn(List.of());
 
-        String token = passwordResetTokenService.createToken(VALID_EMAIL);
+        String token = passwordResetTokenService.createToken(email);
 
         assertThat(token).isNotNull();
 
@@ -48,7 +52,7 @@ class PasswordResetTokenServiceTest {
         assertThat(passwordResetToken.getUser()).isEqualTo(appUser);
 
         InOrder inOrder = inOrder(userService, passwordResetTokenRepository);
-        inOrder.verify(userService).loadUserByEmail(VALID_EMAIL);
+        inOrder.verify(userService).loadUserByEmail(email);
         inOrder.verify(passwordResetTokenRepository).findAllByUserAndConfirmedAtIsNullAndRevokedFalse(appUser);
         inOrder.verify(passwordResetTokenRepository).saveAll(List.of());
     }
@@ -61,11 +65,11 @@ class PasswordResetTokenServiceTest {
         activeToken.setUser(appUser);
         activeToken.setRevoked(false);
 
-        when(userService.loadUserByEmail(VALID_EMAIL)).thenReturn(appUser);
+        when(userService.loadUserByEmail(email)).thenReturn(appUser);
 
         when(passwordResetTokenRepository.findAllByUserAndConfirmedAtIsNullAndRevokedFalse(appUser)).thenReturn(List.of(activeToken));
 
-        String token = passwordResetTokenService.createToken(VALID_EMAIL);
+        String token = passwordResetTokenService.createToken(email);
 
         assertThat(token).isNotNull();
 
@@ -73,10 +77,58 @@ class PasswordResetTokenServiceTest {
         assertThat(activeToken.getConfirmedAt()).isNotNull();
 
 
-        verify(userService).loadUserByEmail(VALID_EMAIL);
+        verify(userService).loadUserByEmail(email);
         verify(passwordResetTokenRepository).findAllByUserAndConfirmedAtIsNullAndRevokedFalse(appUser);
         verify(passwordResetTokenRepository).saveAll(List.of(activeToken));
         verify(passwordResetTokenRepository).save(any(PasswordResetToken.class));
+    }
+
+    @Test
+    void createToken_shouldReturnNull_whenUserDoesNotExist() {
+        when(userService.loadUserByEmail(email)).thenThrow(new UsernameNotFoundException("User not found"));
+
+        String token = passwordResetTokenService.createToken(email);
+
+        assertThat(token).isNull();
+
+        verifyNoInteractions(passwordResetTokenRepository);
+    }
+
+    @Test
+    void validateToken_shouldConfirmAndRevokeToken_whenTokenIsValid() {
+        String rawToken = "rawToken";
+        String hashedToken = TokenHashUtil.getHashedToken(rawToken);
+        AppUser appUser = new AppUser();
+        PasswordResetToken resetToken = new PasswordResetToken();
+        resetToken.setUser(appUser);
+        resetToken.setTokenHash(hashedToken);
+        resetToken.setRevoked(false);
+        resetToken.setExpiresAt(LocalDateTime.now().plusMinutes(1));
+
+        when(passwordResetTokenRepository.findByTokenHash(hashedToken)).thenReturn(Optional.of(resetToken));
+
+        passwordResetTokenService.validateToken(rawToken);
+
+        assertThat(resetToken.isRevoked()).isTrue();
+        assertThat(resetToken.getConfirmedAt()).isNotNull();
+
+        verify(passwordResetTokenRepository).findByTokenHash(hashedToken);
+        verify(passwordResetTokenRepository).save(resetToken);
+    }
+
+    @Test
+    void validateToken_shouldThrowInvalidTokenException_whenTokenDoesNotExist() {
+        String rawToken = "invalidToken";
+        String hashedToken = TokenHashUtil.getHashedToken(rawToken);
+
+        when(passwordResetTokenRepository.findByTokenHash(hashedToken)).thenThrow(new InvalidTokenException("Invalid Token"));
+
+
+        assertThatThrownBy(() -> passwordResetTokenRepository.findByTokenHash(hashedToken))
+                .isInstanceOf(InvalidTokenException.class)
+                .hasMessage("Invalid Token");
+
+        verify(passwordResetTokenRepository, never()).save(any(PasswordResetToken.class));
     }
 
 }
