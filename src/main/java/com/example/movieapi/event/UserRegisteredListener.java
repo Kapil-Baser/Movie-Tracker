6b9a8b05472db65767358a98d8bc3eef99cf450b
Kapil-Baser <kapil.kumar.baser@gmail.com>
2026-02-11 16:23:28 +0530

    Custom Exception that will wrap the MessagingException from JavaMailSender.

--- src/main/java/com/example/movieapi/event/UserRegisteredListener.java
+++ src/main/java/com/example/movieapi/event/UserRegisteredListener.java
@@ -1,7 +1,11 @@
 package com.example.movieapi.event;
 
+import com.example.movieapi.exception.EmailFailedException;
 import com.example.movieapi.service.EmailVerificationService;
+import com.example.movieapi.service.MailService;
+import jakarta.mail.MessagingException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
@@ -10,10 +14,12 @@ import org.springframework.stereotype.Component;
 public class UserRegisteredListener {
 
     private final EmailVerificationService emailVerificationService;
+    private final MailService mailService;
 
-
-    public UserRegisteredListener(EmailVerificationService emailVerificationService) {
+    @Autowired
+    public UserRegisteredListener(EmailVerificationService emailVerificationService, MailService mailService) {
         this.emailVerificationService = emailVerificationService;
+        this.mailService = mailService;
     }
 
     @EventListener
@@ -22,6 +28,15 @@ public class UserRegisteredListener {
         String token = emailVerificationService.createToken(event.user().getEmail());
         log.info("Successfully generated token: {}", token);
         // TODO: Send email to user
+        String to = event.user().getEmail();
+        String name = event.user().getUsername();
+        String link = "http://localhost:8080/auth/confirm?token=" + token;
 
+        try {
+            mailService.sendRegistrationEmail(to, name, link);
+        } catch (MessagingException e) {
+            log.error("Error while sending registration email", e);
+            throw new EmailFailedException(e.getMessage());
+        }
     }
 }
