b5efe3923bd9611ecf34bb281e73b6cb9872c5f9
Kapil-Baser <kapil.kumar.baser@gmail.com>
2026-02-12 09:02:27 +0530

    Sending the password reset email now

--- src/main/java/com/example/movieapi/event/PasswordResetEventListener.java
+++ src/main/java/com/example/movieapi/event/PasswordResetEventListener.java
@@ -1,7 +1,11 @@
 package com.example.movieapi.event;
 
+import com.example.movieapi.exception.EmailFailedException;
+import com.example.movieapi.service.MailService;
 import com.example.movieapi.service.PasswordResetTokenService;
+import jakarta.mail.MessagingException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
@@ -10,18 +14,29 @@ import org.springframework.stereotype.Component;
 public class PasswordResetEventListener {
 
     private final PasswordResetTokenService passwordResetTokenService;
+    private final MailService mailService;
 
-    public PasswordResetEventListener(PasswordResetTokenService resetTokenService) {
+    @Autowired
+    public PasswordResetEventListener(PasswordResetTokenService resetTokenService, MailService mailService) {
         this.passwordResetTokenService = resetTokenService;
+        this.mailService = mailService;
     }
 
     @EventListener
     public void sendResetTokenMail(PasswordResetEvent event) {
-        // TODO mail password reset token
+
         String token = passwordResetTokenService.createToken(event.email());
         if (token != null && !token.isEmpty()) {
-            // TODO: send mail
+            String email = event.email();
+            String link = "http://localhost:8080/auth/resetPassword?token=" + token;
+
+            try {
+                mailService.sendPasswordResetEmail(email, link);
+                log.info("Password reset token: {} for user: {}", token, event.email());
+            } catch (MessagingException e) {
+                log.error("Error while sending registration email", e);
+                throw new EmailFailedException(e.getMessage());
+            }
         }
-        log.info("Password reset token: {} for user: {}", token, event.email());
     }
 }
