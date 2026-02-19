66df0f6131e5e2b06274886399cab187f07e0b02
Kapil-Baser <kapil.kumar.baser@gmail.com>
2026-02-19 19:00:08 +0530

    ...

--- src/test/java/com/example/movieapi/service/UserServiceTest.java
+++ src/test/java/com/example/movieapi/service/UserServiceTest.java
@@ -2,11 +2,13 @@ package com.example.movieapi.service;
 
 import com.example.movieapi.dto.RegisterUserDto;
 import com.example.movieapi.entity.AppUser;
+import com.example.movieapi.entity.Provider;
 import com.example.movieapi.entity.Role;
 import com.example.movieapi.event.UserRegisteredEvent;
 import com.example.movieapi.repository.RoleRepository;
 import com.example.movieapi.repository.UserRepository;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -14,6 +16,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.Optional;
@@ -23,6 +26,8 @@ import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class UserServiceTest {
+
+    static final String USER_EMAIL = "test@example.com";
 
     @Mock
     private UserRepository userRepository;
@@ -39,32 +44,60 @@ class UserServiceTest {
     @InjectMocks
     private UserService userService;
 
+    @BeforeEach
+    void createUser() {
+        AppUser user = new AppUser();
+        user.setUsername("testUsername");
+        user.setPassword("testPassword");
+        user.setEmail(USER_EMAIL);
+        user.setEnabled(false);
+        user.setProvider(Provider.LOCAL);
+        user.setProviderId(null);
+    }
+
     @Test
     void existsByEmail_shouldReturnTrue_whenEmailExists() {
         // given
-        String email = "text@example.com";
-        when(userRepository.existsByEmail(email)).thenReturn(true);
+        when(userRepository.existsByEmail(USER_EMAIL)).thenReturn(true);
 
         // when
-        boolean result = userService.existsByEmail(email);
+        boolean result = userService.existsByEmail(USER_EMAIL);
 
         // then
         assertThat(result).isTrue();
-        verify(userRepository).existsByEmail(email);
+        verify(userRepository).existsByEmail(USER_EMAIL);
+    }
+
+    @Test
+    void existsByEmail_shouldReturnFalse_whenEmailDoesNotExists() {
+        when(userRepository.existsByEmail(anyString())).thenReturn(false);
+
+        boolean result = userService.existsByEmail("test");
+        assertThat(result).isFalse();
+        verify(userRepository).existsByEmail("test");
+    }
+
+    @Test
+    void existsByEmail_shouldReturnFalse_whenEmailIsNull() {
+        when(userRepository.existsByEmail(null)).thenReturn(false);
+
+        boolean result = userService.existsByEmail(null);
+        assertThat(result).isFalse();
+        verify(userRepository).existsByEmail(null);
     }
 
     @Test
     void findByEmail_shouldReturnUser_whenEmailExists() {
         // given
         String email = "user@example.com";
-        AppUser appUser = new AppUser("test-user", email, "test1234", true, new Role() );
-        when(userRepository.findByEmail(email)).thenReturn(Optional.of(appUser));
+        AppUser user = new AppUser();
+        when(userRepository.findByEmail(email)).thenReturn(Optional.of(user));
 
         // when
         AppUser returnedUser = userService.loadUserByEmail(email);
 
         // then
-        assertThat(returnedUser).isEqualTo(appUser);
+        assertThat(returnedUser).isEqualTo(user);
         verify(userRepository).findByEmail(email);
     }
 
@@ -86,11 +119,11 @@ class UserServiceTest {
     }
 
     @Test
-    void isValidPassword_returnsTrue_whenPasswordIsValid() {
+    void isValidPassword_returnsTrue_whenPasswordMatches() {
         // given
         String rawPassword = "test1234";
         String encodedPassword = "test1234";
-        AppUser appUser = new AppUser("test-user", "user@example.com", encodedPassword, true, new Role() );
+        AppUser appUser = new AppUser();
         when(passwordEncoder.matches(rawPassword, encodedPassword)).thenReturn(true);
 
         // when
@@ -98,6 +131,20 @@ class UserServiceTest {
 
         // then
         assertThat(result).isTrue();
+        verify(passwordEncoder).matches(rawPassword, encodedPassword);
+    }
+
+    @Test
+    void isValidPassword_returnsFalse_whenPasswordDoesNotMatches() {
+        String rawPassword = "test1234";
+        String encodedPassword = "encodedtest1234";
+        AppUser user = new AppUser();
+        user.setPassword(encodedPassword);
+        when(passwordEncoder.matches(rawPassword, encodedPassword)).thenReturn(false);
+
+        boolean result = userService.isValidPassword(user, rawPassword);
+
+        assertThat(result).isFalse();
         verify(passwordEncoder).matches(rawPassword, encodedPassword);
     }
 
