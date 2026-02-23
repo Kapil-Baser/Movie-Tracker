e89f02d57625d4f6207326d69e78a40c59d28faa
Kapil-Baser <kapil.kumar.baser@gmail.com>
2026-02-23 12:35:38 +0530

    2 more tests for watched movie service class

--- src/test/java/com/example/movieapi/service/WatchedMovieServiceTest.java
+++ src/test/java/com/example/movieapi/service/WatchedMovieServiceTest.java
@@ -1,5 +1,6 @@
 package com.example.movieapi.service;
 
+import com.example.movieapi.dto.WatchedMovieDto;
 import com.example.movieapi.entity.AppUser;
 import com.example.movieapi.entity.Movie;
 import com.example.movieapi.entity.WatchedMovie;
@@ -12,7 +13,11 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.Optional;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.*;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -54,5 +59,88 @@ class WatchedMovieServiceTest {
         assertThat(watchedMovie.getWatchedAt()).isNotNull();
 
         verify(watchedMovieRepository, never()).delete(watchedMovie);
+    }
+
+    @Test
+    void toggleWatched_shouldDeleteRecord_whenAlreadyWatched() {
+        AuthenticatedUser authenticatedUser = new AuthenticatedUser(new AppUser());
+        Long  movieId = 1L;
+        Movie movie = new Movie();
+        movie.setId(movieId);
+
+        WatchedMovie watchedMovie = new WatchedMovie();
+        watchedMovie.setMovie(movie);
+        watchedMovie.setUser(authenticatedUser.getUser());
+
+        when(movieService.getMovieById(movieId)).thenReturn(movie);
+
+        when(watchedMovieRepository.findByUserAndMovie(authenticatedUser.getUser(), movie)).thenReturn(Optional.of(watchedMovie));
+
+        boolean result = watchedMovieService.toggleWatched(authenticatedUser, movieId);
+
+        assertThat(result).isFalse();
+        verify(watchedMovieRepository).delete(watchedMovie);
+        verify(watchedMovieRepository, never()).save(watchedMovie);
+    }
+
+    @Test
+    void toggleWatched_shouldPropagateException_whenMovieNotFound() {
+        AuthenticatedUser authenticatedUser = new AuthenticatedUser(new AppUser());
+
+        when(movieService.getMovieById(anyLong())).thenThrow(RuntimeException.class);
+
+        assertThatThrownBy(() -> watchedMovieService.toggleWatched(authenticatedUser, 1L)).isInstanceOf(RuntimeException.class);
+
+        verifyNoInteractions(watchedMovieRepository);
+    }
+
+    @Test
+    void getWatchedMovies_shouldReturnEmptyMap_whenUserHasNoWatchedMovies() {
+        AuthenticatedUser authenticatedUser = new AuthenticatedUser(new AppUser());
+
+        when(watchedMovieRepository.findByUserOrderByWatchedAtDesc(authenticatedUser.getUser())).thenReturn(Collections.emptyList());
+
+        Map<LocalDate, List<WatchedMovieDto>> watchedMovies = watchedMovieService.getWatchedMovies(authenticatedUser);
+
+        assertThat(watchedMovies).isEmpty();
+    }
+
+    @Test
+    void getWatchedMovies_shouldMapEntityToDtoCorrectly() {
+        AuthenticatedUser authenticatedUser = new AuthenticatedUser(new AppUser());
+        Movie movie = new Movie();
+        movie.setId(1L);
+        movie.setTitle("title");
+        movie.setPosterPath("/path/to/image");
+        movie.setRuntime(139);
+        movie.setTmdbGenres(Set.of("Action", "Drama"));
+        movie.setRating(BigDecimal.valueOf(8.0));
+        movie.setCertification("R");
+        movie.setReleaseDate(LocalDate.of(2025, 12, 10));
+        movie.setVotes(999L);
+
+        WatchedMovie watchedMovie = new WatchedMovie();
+        watchedMovie.setMovie(movie);
+        watchedMovie.setUser(authenticatedUser.getUser());
+        watchedMovie.setWatchedAt(LocalDateTime.of(LocalDate.now(), LocalTime.MIDNIGHT));
+
+        when(watchedMovieRepository.findByUserOrderByWatchedAtDesc(authenticatedUser.getUser())).thenReturn(List.of(watchedMovie));
+
+        Map<LocalDate, List<WatchedMovieDto>> watchedMovies = watchedMovieService.getWatchedMovies(authenticatedUser);
+
+        assertThat(watchedMovies).isNotEmpty();
+
+        WatchedMovieDto watchedMovieDto = watchedMovies.get(watchedMovie.getWatchedAt().toLocalDate()).getFirst();
+
+        assertThat(watchedMovieDto.getTitle()).isEqualTo(movie.getTitle());
+        assertThat(watchedMovieDto.getPosterPath()).isEqualTo(movie.getPosterPath());
+        assertThat(watchedMovieDto.getRating()).isEqualTo(movie.getRating());
+        assertThat(watchedMovieDto.getCertification()).isEqualTo(movie.getCertification());
+        assertThat(watchedMovieDto.getReleaseDate()).isEqualTo("Dec 10, 2025");
+        assertThat(watchedMovieDto.getRuntime()).isEqualTo("2h 19min");
+        assertThat(watchedMovieDto.getWatchedAt()).isEqualTo(watchedMovie.getWatchedAt().toLocalDate());
+        assertThat(watchedMovieDto.getGenres()).isEqualTo(movie.getTmdbGenres());
+        assertThat(watchedMovieDto.getVotes()).isEqualTo(movie.getVotes());
+        assertThat(watchedMovieDto.getMovieId()).isEqualTo(String.valueOf(watchedMovie.getMovie().getId()));
     }
 }
