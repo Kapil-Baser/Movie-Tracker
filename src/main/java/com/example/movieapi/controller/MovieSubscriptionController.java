ab2c51e8bf367cb53442a58eb8cebf5cc674ea31
Kapil-Baser <kapil.kumar.baser@gmail.com>
2026-04-13 16:24:58 +0530

    Added two fragments now, and it will show a toast notification if movie is subscribed or not

--- src/main/java/com/example/movieapi/controller/MovieSubscriptionController.java
+++ src/main/java/com/example/movieapi/controller/MovieSubscriptionController.java
@@ -1,25 +1,31 @@
 package com.example.movieapi.controller;
 
 import com.example.movieapi.model.AuthenticatedUser;
+import com.example.movieapi.repository.MoviesRepository;
 import com.example.movieapi.service.MovieSubscriptionService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.view.FragmentsRendering;
 
 @Controller
 @RequestMapping("/user/subscribe")
 public class MovieSubscriptionController {
 
     private final MovieSubscriptionService subscriptionService;
+    private final MoviesRepository moviesRepository;
 
-    public MovieSubscriptionController(MovieSubscriptionService subscriptionService) {
+    @Autowired
+    public MovieSubscriptionController(MovieSubscriptionService subscriptionService, MoviesRepository moviesRepository) {
         this.subscriptionService = subscriptionService;
+        this.moviesRepository = moviesRepository;
     }
 
-    @PostMapping("/toggle")
+    /*@PostMapping("/toggle")
     public String toggleSubscription(@RequestParam Long movieId,
                                      @AuthenticationPrincipal AuthenticatedUser authenticatedUser,
                                      Model model) {
@@ -29,5 +35,30 @@ public class MovieSubscriptionController {
         model.addAttribute("isSubscribed", isSubscribed);
 
         return "fragments/buttons :: subscribe-to-movie";
+    }*/
+
+    @PostMapping("/toggle")
+    public FragmentsRendering toggleSubscription(@RequestParam Long movieId,
+                                                 @AuthenticationPrincipal AuthenticatedUser authenticatedUser,
+                                                 Model model) {
+
+        boolean isSubscribed = subscriptionService.toggleSubscription(authenticatedUser, movieId);
+        String movieTitle = moviesRepository.findTitleByMovieId(movieId);
+
+        if (isSubscribed) {
+            model.addAttribute("message", "You will be notified when '" + movieTitle + "' is out for streaming.");
+        } else {
+            model.addAttribute("message", "Unsubscribed from being notified when '" + movieTitle + "' is out for streaming.");
+        }
+
+        model.addAttribute("isSubscribed", isSubscribed);
+        model.addAttribute("movieId", movieId);
+
+
+        return FragmentsRendering
+                .fragment("fragments/buttons :: subscribe-to-movie")
+                .fragment("fragments/toasts :: hx-success")
+                .build();
+
     }
 }
