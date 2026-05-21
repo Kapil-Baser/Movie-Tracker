12ab29315cdbebae09ce929dcd051d8c880de380
Kapil-Baser <kapil.kumar.baser@gmail.com>
2026-05-21 17:42:33 +0530

    Added a method to get paged results of movies

--- src/main/java/com/example/movieapi/repository/MoviesRepository.java
+++ src/main/java/com/example/movieapi/repository/MoviesRepository.java
@@ -14,7 +14,7 @@ import java.util.List;
 @Repository
 public interface MoviesRepository extends JpaRepository<Movie, Long> {
 
-    List<Movie> findByTitleContainingIgnoreCase(String title);
+    Page<Movie> findByTitleContainingIgnoreCase(String title, Pageable pageable);
 
     List<Movie> findByUsDigitalDateIsNull();
 
