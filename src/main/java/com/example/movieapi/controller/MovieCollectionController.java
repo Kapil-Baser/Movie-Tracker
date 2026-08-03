84d2c72294e77bad47521e40ddfcdb3aca97c5e7
Kapil-Baser <kapil.kumar.baser@gmail.com>
2026-08-03 09:49:57 +0530

    Removed the class level method authentication since it's not enabled

--- src/main/java/com/example/movieapi/controller/MovieCollectionController.java
+++ src/main/java/com/example/movieapi/controller/MovieCollectionController.java
@@ -11,7 +11,6 @@ import io.github.wimdeblauwe.htmx.spring.boot.mvc.HxRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -21,7 +20,6 @@ import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/collections")
-@PreAuthorize("isAuthenticated()")
 @RequiredArgsConstructor
 public class MovieCollectionController {
 
