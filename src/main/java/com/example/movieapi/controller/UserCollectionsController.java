d340a9b96d8e181734c8a205da67944075f68e47
Kapil-Baser <kapil.kumar.baser@gmail.com>
2026-08-18 12:21:52 +0530

    Added methods for showing the collection rename form and handing the renaming of collection.

--- src/main/java/com/example/movieapi/controller/UserCollectionsController.java
+++ src/main/java/com/example/movieapi/controller/UserCollectionsController.java
@@ -1,16 +1,20 @@
 package com.example.movieapi.controller;
 
-import com.example.movieapi.dto.CollectionOptionDto;
+import com.example.movieapi.dto.CollectionRenameDto;
+import com.example.movieapi.dto.MovieCollectionView;
 import com.example.movieapi.dto.SelectedCollectionDto;
+import com.example.movieapi.entity.AppUser;
 import com.example.movieapi.model.AuthenticatedUser;
 import com.example.movieapi.service.MovieCollectionService;
 import com.example.movieapi.service.MovieViewAssemblerService;
 import io.github.wimdeblauwe.htmx.spring.boot.mvc.HxRequest;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -29,23 +33,24 @@ public class UserCollectionsController {
     }
 
     @GetMapping("/collections")
-    public String showUserCollectionForm(@RequestParam("movie_id") Long movieId,
+    public String showAddMovieToUserCollectionForm(@RequestParam("movie_id") Long movieId,
                                          @RequestParam("movie_title") String movieTitle,
                                          @AuthenticationPrincipal AuthenticatedUser authenticatedUser,
                                          Model model) {
 
-        List<CollectionOptionDto> collections = movieViewAssemblerService.buildCollectionOptions(authenticatedUser, movieId);
+        List<MovieCollectionView> movieCollectionViews = movieViewAssemblerService.buildMovieCollectionView(authenticatedUser, movieId);
 
         // Pre-populating the movieId, which I want to carry over to the post request of form
         SelectedCollectionDto dto = new SelectedCollectionDto();
         dto.setSelectedMovieId(movieId);
         dto.setSelectedMovieTitle(movieTitle);
 
-        model.addAttribute("userCollections", collections);
+        model.addAttribute("movieCollectionViews", movieCollectionViews);
         model.addAttribute("selectedCollection", dto);
         return "fragments/modal :: movie-collection-form";
     }
 
+    // TODO: Maybe here also check the authenticated user is adding a movie to a collection they own
     @HxRequest
     @PostMapping("/collections/add-movie")
     public String addMovieToCollection(@ModelAttribute SelectedCollectionDto dto, Model model) {
@@ -62,17 +67,66 @@ public class UserCollectionsController {
 
     @HxRequest
     @GetMapping("/collections/delete-config")
-    public String showDeleteCollectionConfirmation(@RequestParam("collection_id") Long collectionId,
+    public String showDeleteCollectionConfirmation(@RequestParam("collectionId") Long collectionId,
                                                    Model model) {
         model.addAttribute("collectionId", collectionId);
         return "fragments/buttons :: modal-delete";
     }
 
     @HxRequest
-    @DeleteMapping("/collections/delete")
-    public ResponseEntity<Void> deleteCollection(@RequestParam(name = "collection_id") Long collectionId,
+    @DeleteMapping("/collections/{collectionId}")
+    public ResponseEntity<Void> deleteCollection(@PathVariable Long collectionId,
                                                  @AuthenticationPrincipal AuthenticatedUser authenticatedUser) {
-        movieCollectionService.deleteCollectionByUserAndId(authenticatedUser, collectionId);
+        movieCollectionService.deleteCollectionByUserAndId(authenticatedUser.getUser(), collectionId);
         return ResponseEntity.ok().build();
+    }
+
+    @HxRequest
+    @GetMapping("/collections/{collectionId}/edit")
+    public String renameCollectionForm(@PathVariable(name = "collectionId") Long collectionId,
+                                   @AuthenticationPrincipal AuthenticatedUser authenticatedUser,
+                                   Model model) {
+        String collectionName = movieCollectionService.getCollectionName(collectionId);
+
+        CollectionRenameDto renameDto = new CollectionRenameDto(collectionName, collectionId);
+        model.addAttribute("collectionId", collectionId);
+        model.addAttribute("collection", renameDto);
+
+
+        return "fragments/collection-card :: collectionRename";
+    }
+
+    @HxRequest
+    @PutMapping("/collections/{collectionId}")
+    public String renameCollection(@Valid @ModelAttribute("collection") CollectionRenameDto renameDto,
+                                   BindingResult bindingResult,
+                                   @PathVariable Long collectionId,
+                                   @AuthenticationPrincipal AuthenticatedUser authenticatedUser,
+                                   Model model) {
+
+        if (bindingResult.hasErrors()) {
+            // Returning the form with errors
+            model.addAttribute("collectionId", collectionId);
+            model.addAttribute("collection", renameDto);
+            return "fragments/collection-card :: collectionRename";
+        }
+
+        AppUser user = authenticatedUser.getUser();
+
+        if (movieCollectionService.nameExists(renameDto.getName(), user.getId())) {
+            bindingResult.rejectValue("name", "name.duplicate", "This collection name already exists");
+            model.addAttribute("collection", renameDto);
+            return "fragments/collection-card :: collectionRename";
+        }
+        return "fragments/collection-card :: collectionName";
+    }
+
+    @HxRequest
+    @GetMapping("/collections/{collectionId}/edit/cancel")
+    public String cancelCollectionRename(@PathVariable Long collectionId, Model model) {
+        String collectionName = movieCollectionService.getCollectionName(collectionId);
+        model.addAttribute("collectionName", collectionName);
+        model.addAttribute("collectionId", collectionId);
+        return "fragments/collection-card :: collectionName";
     }
 }
