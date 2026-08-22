ac80fe49f97a11dcd7ed6b50cbfb60f1bb58fe5a
Kapil-Baser <kapil.kumar.baser@gmail.com>
2026-08-22 11:44:43 +0530

    Added some controller methods for mdblist and getting trakt trending and tmdb movie summary

--- src/main/java/com/example/movieapi/controller/ApiController.java
+++ src/main/java/com/example/movieapi/controller/ApiController.java
@@ -1,18 +1,19 @@
 package com.example.movieapi.controller;
 
-import com.example.movieapi.model.response.MovieResultResponse;
+import com.example.movieapi.model.mdblist.MdbListMovies;
+import com.example.movieapi.model.response.TmdbMovieDetailsResponse;
 import com.example.movieapi.model.response.TmdbReleaseDatesResponse;
 import com.example.movieapi.model.response.TmdbDiscoverResponse;
 import com.example.movieapi.model.response.TmdbTrendingMoviesResponse;
 import com.example.movieapi.model.trakt.model.TraktMovie;
 import com.example.movieapi.model.trakt.response.TraktAllVideosResponse;
+import com.example.movieapi.service.MdbListService;
 import com.example.movieapi.service.TmdbService;
 import com.example.movieapi.service.TraktService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDate;
 import java.util.List;
 
 
@@ -22,11 +23,23 @@ public class ApiController {
 
     private final TmdbService tmdbService;
     private final TraktService traktService;
+    private final MdbListService mdbListService;
 
     @Autowired
-    public ApiController(TmdbService tmdbService, TraktService traktService) {
+    public ApiController(TmdbService tmdbService, TraktService traktService, MdbListService mdbListService) {
         this.tmdbService = tmdbService;
         this.traktService = traktService;
+        this.mdbListService = mdbListService;
+    }
+
+    @GetMapping("/mdblist/movie/horror")
+    public ResponseEntity<String> getHorrorFromMdbList() {
+        return ResponseEntity.ok(mdbListService.getHorrorMovies());
+    }
+
+    @GetMapping("/mdblist/movie/list")
+    public ResponseEntity<MdbListMovies> getOfficialList() {
+        return ResponseEntity.ok(mdbListService.getOfficialList());
     }
 
     @GetMapping("/tmdb/trending/{time_window}")
@@ -49,11 +62,6 @@ public class ApiController {
         return ResponseEntity.ok(traktService.getAllVideos(movieId));
     }
 
-    @GetMapping("/tmdb/discover")
-    public ResponseEntity<List<MovieResultResponse>> discoverMovie() {
-        return ResponseEntity.ok(tmdbService.discoverMovies(2026, LocalDate.now().withDayOfMonth(1),4));
-    }
-
     @GetMapping("/tmdb/now_playing/{page_no}")
     public ResponseEntity<TmdbTrendingMoviesResponse> trendingMoviesTmdb(@PathVariable(name = "page_no") Integer page) {
         return ResponseEntity.ok(tmdbService.getTrendingMovies(page));
@@ -62,5 +70,16 @@ public class ApiController {
     @GetMapping("/trakt/summary/{tmdb_id}")
     public ResponseEntity<TraktMovie> getMovieSummary(@PathVariable("tmdb_id") String tmdbId) {
         return ResponseEntity.ok(traktService.getExtendedMovieDetails(tmdbId));
+    }
+
+    @GetMapping("/tmdb/summary/{tmdb_id}")
+    public ResponseEntity<TmdbMovieDetailsResponse> getTmdbSummary(@PathVariable("tmdb_id") Long tmdbId) {
+        var result = tmdbService.getMovieDetailsExtended(tmdbId);
+        return ResponseEntity.ok(result);
+    }
+
+    @GetMapping("/trakt/trending")
+    public String getTrendingFromTrakt() {
+        return traktService.getTrendingMoviesApi();
     }
 }
