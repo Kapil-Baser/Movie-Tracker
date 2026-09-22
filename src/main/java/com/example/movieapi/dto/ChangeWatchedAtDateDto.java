a135eb3dc5c4045ea78c30d71057d2f6337bd627
Kapil-Baser <kapil.kumar.baser@gmail.com>
2026-09-22 11:51:50 +0530

    Renamed from ChangeWatchedOnDateDto to ChangeWatchedAtDateDto.

--- src/main/java/com/example/movieapi/dto/ChangeWatchedAtDateDto.java
+++ src/main/java/com/example/movieapi/dto/ChangeWatchedAtDateDto.java
@@ -9,7 +9,7 @@ import java.time.LocalDate;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class ChangeWatchedOnDateDto {
+public class ChangeWatchedAtDateDto {
     private LocalDate oldWatchedDate;
     private LocalDate newWatchedDate;
 }
